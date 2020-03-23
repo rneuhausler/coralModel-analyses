d64cc3c28aa513eaf0ff0294b35b49fbd0566429
Rosanna Neuhausler <rneuhausler@berkeley.edu>
2020-03-23 16:06:38 -0700

    changes to mostly functions, neater script

--- scripts/coralModelTest.py
+++ scripts/coralModelTest.py
@@ -1,129 +1,132 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-## Libraries
 from coralModel import Reef, Organism, Ocean
+import tools as tl
 import numpy as np
 import sys
-import math
-import matplotlib.pyplot as plt 
-from multiprocessing import Pool
-import pickle
+import matplotlib.pyplot as plt
 import pandas as pd
+from multiprocessing import Pool
 
-## Input Parameters
+##  Parameters
 
-nProcessors = 4
-threshold = 1.45                            
-coralPercent = .33
-algaePercent = .33
-r=1.0
-d=.4 
-a=.2
-y=.75
-g=.4
-
-#Time and Grid Settings
-NumberOfSimulations = 100
-tf, dt= 20, .1 
-rows, columns = 15, 15
-
-#Also need blobValue
-gridOption = 0
-
-
-## Calculated from Input Parameters
-
-turfPercent = 1 - coralPercent - algaePercent
+nProcessors = int(sys.argv[1])
+NumberOfSimulations = int(sys.argv[2])
+coralPercent = float(sys.argv[3])
+algaePercent = float(sys.argv[4])
+gridOption = int(sys.argv[5])
+rows, columns = int(sys.argv[6]), int(sys.argv[7])
+threshold = float(sys.argv[8])
+recordRate = int(sys.argv[9])
+r, d, a, y, g, dt, tf = [float(sys.argv[n]) for n in range(10,17)]
 NumberOfTimesteps = int(tf/dt)
+NumberOfRecordedTimesteps = round(NumberOfTimesteps / recordRate)
 NumberOfNodes = rows * columns
+turfPercent = 1 - coralPercent - algaePercent
 
-## Grid Options Setup
-if gridOption == 1: #setup checkered
+
+##  Functions
+
+def generateCheckerBoard():
     m = rows + 2
     n = columns + 2
-    checkerBoard = np.tile(np.array([[0,1,2],[1,2,0],[2,0,1]]), ((m+2)//3, (n+2)//3))
-    
-elif gridOption == 2: #setup blob
-    
-    blobValue = 0 #input system argument
-    notBlob = [a for a in [0,1,2] if a != blobValue]
-    
+    checkerBoard = np.tile(np.array([[0,1,2],[1,2,0],[2,0,1]]), 
+                           ((m+2)//3, (n+2)//3))
+    return(checkerBoard)
+
+def generateBlob(blobPercent):
+    blobValue = int(sys.argv[18])
+    notBlob = [a for a in [0,1,2] if a != blobValue]   
     center = (rows/2, columns/2)
     distanceGrid = np.array([Reef.distance([i+.5,j+.5], center)
                              for i in range(0,rows)
                              for j in range(0,columns)])
-    maxDistance = np.sort(distanceGrid)[round(coralPercent*NumberOfNodes)]
+    maxDistance = np.sort(distanceGrid)[round(blobPercent*NumberOfNodes)]
     blobLocations = (np.where(distanceGrid.reshape(rows,columns) < maxDistance))
-    blobLocations = [(blobLocations[0][n],blobLocations[1][n]) for n in range(0,len(blobLocations[0]))]
-
-data = np.zeros((NumberOfTimesteps, NumberOfNodes))
-
-## Model Run Function
-
-def runModel(simulation):
-    print('running simulation' + str(simulation))
-    Moorea = Reef()
-    count = 0
+    blobLocations = [(blobLocations[0][n],blobLocations[1][n]) 
+                     for n in range(0,len(blobLocations[0]))]
+    return(blobLocations, notBlob)
     
-    ## Creating the reef
+def createReef():
+    Moorea = Reef()
+    count = 0   
     for i in range(0,rows):
         for j in range(0,columns):
             
             if gridOption == 0:
-                U = np.random.choice([0,1,2],
-                                     p=[coralPercent, turfPercent, algaePercent])
+                U = np.random.choice([0,1,2], p=[coralPercent, 
+                                                 turfPercent, algaePercent])
             elif gridOption == 1:
                 U = checkerBoard[i,j]
-            
+                
             elif gridOption == 2:
                 if (i,j) in blobLocations: 
                     U = blobValue
                 else:
-                    U = np.random.choice(notBlob,
-                                         p=[.5, .5])
+                    U = np.random.choice(notBlob, p=[.5, .5])
             node = Organism(type=U, location=[i,j], ID=count)
             Moorea.append(node)
             count = count + 1
-    Moorea.generateGraph(threshold)
+    return(Moorea)
+
+def densityExtract(Moorea, Type, Count):
+    if Count == 0:
+        neighbors = 0
+    else:
+        neighbors = np.array([Moorea.nodes[n].density/
+                              Moorea.nodes[n].density.sum()
+                              for n,val in enumerate(Moorea.nodes)
+                              if Moorea.nodes[n].type == Type]).mean(axis=0)[Type]
+    return(neighbors)
+
+def pullInfo(Moorea, simulation, timestep):
+    image = np.array([Moorea.nodes[n].type
+                      for n,val in enumerate(Moorea.nodes)])
+    C = np.count_nonzero(image==0)
+    CN = densityExtract(Moorea, int(0), C)
+    T = np.count_nonzero(image==1)
+    TN = densityExtract(Moorea, int(1), T)
+    M = np.count_nonzero(image==2)
+    MN = densityExtract(Moorea, int(2), M)
+    CP, AP, TP, MP = tl.patchCounts(image, rows)
+    data = [simulation, timestep, C, T, M, CN, TN, MN, CP, AP, TP, MP]  
+    dataframe = pd.DataFrame([data])
+    return(dataframe)
+
+def runModel(simulation):
     
-    ## Running new reef through time
-    for n in range(0,NumberOfTimesteps):
-        for i,val in enumerate(Moorea.nodes):
-            data[n,i] = Moorea.nodes[i].type
+    print('running simulation' + str(simulation))
+    
+    Moorea = createReef()
+    Moorea.generateGraph(threshold) 
+    
+    for timestep in range(0,NumberOfTimesteps):
+        if timestep == 0:
+            table = pd.DataFrame([])
+        if timestep % recordRate == 0:
+            table = pd.concat([table, pullInfo(Moorea, simulation, timestep)])      ## 
+            
         Moorea.roll(r=r, d=d, a=a, g=g, y=y, dt=dt)
-    
-    if simulation = 0: ## check on progress (may add first and last as well)
-        plt.figure()
-        plt.imshow(np.reshape(np.array([Moorea.nodes[i].type for
-                                        i,val in enumerate(Moorea.nodes)]),(rows,columns)))
-    return(data)
- 
-## Run Model
-    
-if __name__ == '__main__':
         
+    return(table)
+
+## Grid Options Setup
+
+if __name__ == '__main__':
+    
+    if gridOption == 1:
+        checkerBoard = generateCheckerBoard(rows, columns)
+    elif gridOption == 2:
+        blobLocations, notBlob = generateBlob(coralPercent)    
+    
     with Pool(nProcessors) as p:
-        results = p.map(runModel, np.arange(NumberOfSimulations)) ## function, argument
-
-## Save Output File
-
-path = 'outputFolder/'
-output_name = 'parameterValues'
-outfile = open(path+output_name, "wb") #open pickle jar
-pickle.dump(results, outfile)           #put contents into jar
-outfile.close()                        #close the jar
-
-## Pull some information (using parallelization) and save into pandas table
-## Add CN, CNr
-
-## save csv files
-path = 'outputFolder/'
-output_name = 'summary'
-np.savetxt(path+ output_name + '.csv', types, delimiter=",")
-
-
-
-## histograms - count, neighbors, npatches - nice to compare different initial grids
-##looking at all the simulations at different timesteps (reached equilibrium if looking the same after 1000 timesteps)
-
+        output = pd.concat(p.map(runModel, np.arange(NumberOfSimulations)))     ##
+        
+    output.columns = ['Simulation', 'Timestep', 'CoralCount', 'TurfCount',
+                      'MacroalgaeCount', 'Coral-CoralNeighbors', 
+                      'Turf-TurfNeighbors', 'Macro-MacroNeighbors',
+                      'CoralPatchCount', 'AlgaePatchCount', 'TurfPatchCount',
+                      'MacroPatchCount']
+    
+    output.to_csv('table.csv', header=True, index=False)
